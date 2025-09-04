3f1e8c324c8242ae9d3a312f500cace7660c410b
Grefred <133239181+Gr1X@users.noreply.github.com>
2025-09-04 21:15:46 +0700

    Commit No. 4 – Standard activity and single top activity

--- app/src/main/java/com/umn/lab_week_02_c/MainActivity.kt
+++ app/src/main/java/com/umn/lab_week_02_c/MainActivity.kt
@@ -1,15 +1,52 @@
 package com.umn.lab_week_02_c
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val DEBUG_TAG = "MainActivityDebug"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // Called enableEdgeToEdge
         setContentView(R.layout.activity_main)
+
+        // Apply window insets listener for edge-to-edge
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Log.d(DEBUG_TAG, "onCreate")
+
+        val buttonClickListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.button_standard -> {
+                    Log.d(DEBUG_TAG, "Standard Activity button clicked")
+                    startActivity(
+                        Intent(this, StandardActivity::class.java)
+                    )
+                }
+                R.id.button_single_top -> {
+                    Log.d(DEBUG_TAG, "SingleTop Activity button clicked")
+                    startActivity(
+                        Intent(this, SingleTopActivity::class.java)
+                    )
+                }
+            }
+        }
+
+        findViewById<Button>(R.id.button_standard).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button_single_top).setOnClickListener(buttonClickListener)
     }
 }
